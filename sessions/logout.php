a2fd11a589b37940e97354784e0f3f58fa5dcecb
vshapovalov <kupper133@gmail.com>
2021-09-10 14:51:34 +0300

    delete(sessions): delete unused folder

--- sessions/logout.php
+++
@@ -1,3 +0,0 @@
-<?php if(session_destroy()){echo "сессия успешно разрушена";} 
-var_dump($_SESSION);?>
-<a href="main.php"><button>Вернуться на главную.</button></a>
--- sessions/testn1.php
+++
@@ -1,5 +0,0 @@
-<form action="testn2.php" method="GET">
-	<p>Введите ваше имя: <br><input type="text" name="username"></p>
-	<p>Введите ваш возраст: <br><input type="text" name="userage"></p>
-	<p><input type="submit" name="submit"></p>
-</form>
